4d92e4c889735ad14d160c4dbc204800b0a48f60
mksrkv <maximkarykov@gmail.com>
2023-10-30 21:13:53 +0300

    last commit

--- 02/2.go
+++ 02/2.go
@@ -11,10 +11,12 @@ func main() {
 	var wg = sync.WaitGroup{}
 	for _, v := range array {
 		wg.Add(1)
-		go func(v int) {
-			defer wg.Done()
-			fmt.Printf("%d: %f | ", v, math.Pow(float64(v), 2))
-		}(v)
+		go increaseAndPrint(float64(v), &wg)
 	}
 	wg.Wait()
+}
+
+func increaseAndPrint(num float64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("%d: %f | ", int(num), math.Pow(float64(num), 2))
 }
