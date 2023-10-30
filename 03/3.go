1986cc27bb28f1d9c51b8758b2894c6e6daad304
mksrkv <maximkarykov@gmail.com>
2023-10-30 22:07:45 +0300

    last commit

--- 03/3.go
+++ 03/3.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	array := [5]int{2, 4, 6, 8, 10}
+	wg := sync.WaitGroup{}
+	sum := 0
+	for _, v := range array {
+		wg.Add(1)
+		go squareAndPow(v, &sum, &wg)
+	}
+	wg.Wait()
+	fmt.Print(sum)
+}
+
+func squareAndPow(num int, sum *int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	*sum += num * num
 }
